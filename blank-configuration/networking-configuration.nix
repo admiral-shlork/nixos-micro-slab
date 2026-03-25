24138c6cf445d12ecfc0dc731c6add81b9c967d8
admiral-shlork <vvhtvr@protonmail.ch>
2026-03-25 12:40:57 +0000

    unblocked more ports in fw

--- blank-configuration/networking-configuration.nix
+++ blank-configuration/networking-configuration.nix
@@ -5,7 +5,7 @@
     hostName = "micro-slab";
     useDHCP = lib.mkDefault true;
     networkmanager.enable = true;
-    firewall.allowedTCPPorts = [ 80 8080 6443 ];
+    firewall.allowedTCPPorts = [ 80 8080 6443 10257 10259 ];
     firewall.allowedUDPPorts = [ ];
   };
 }
